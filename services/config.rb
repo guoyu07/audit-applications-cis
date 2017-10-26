5412940d7e8861260e24a293660a42e6edac511b
Nandesh <nandesh@cloudcoreo.com>
2017-10-26 13:23:40 -0700

    formatting code

--- services/config.rb
+++ services/config.rb
@@ -1,40 +1,40 @@
 coreo_agent_selector_rule 'check-docker' do
-    action :define
-    timeout 30
-    control 'check-docker' do
-      describe command('docker') do
-         it { should exist }
-      end
+  action :define
+  timeout 30
+  control 'check-docker' do
+    describe command('docker') do
+        it { should exist }
     end
   end
+end
 
 coreo_agent_audit_profile 'cis-docker-benchmark' do
-    action :define
-    selectors ['check-docker']
-    profile 'https://github.com/dev-sec/cis-docker-benchmark/archive/master.zip'
-    timeout 120
-  end
+  action :define
+  selectors ['check-docker']
+  profile 'https://github.com/dev-sec/cis-docker-benchmark/archive/master.zip'
+  timeout 120
+end
 
-  coreo_agent_selector_rule 'check-kubectl' do
-    action :define
-    timeout 30
-    control 'check-kubectl' do
-      describe command('kubectl') do
-         it { should exist }
-      end
+coreo_agent_selector_rule 'check-kubectl' do
+  action :define
+  timeout 30
+  control 'check-kubectl' do
+    describe command('kubectl') do
+        it { should exist }
     end
   end
+end
 
 coreo_agent_audit_profile 'cis-kubernetes-benchmark' do
-    action :define
-    selectors ['check-kubectl']
-    profile 'https://github.com/dev-sec/cis-kubernetes-benchmark/archive/master.zip'
-    timeout 120
-  end
+  action :define
+  selectors ['check-kubectl']
+  profile 'https://github.com/dev-sec/cis-kubernetes-benchmark/archive/master.zip'
+  timeout 120
+end
 
-  coreo_agent_rule_runner 'agent-rules' do
-    action :run
-    rules ${AUDIT_AGENT_RULES_ALERT_LIST}
-    profiles ${AUDIT_AGENT_PROFILES_ALERT_LIST}
-    filter(${FILTERED_OBJECTS}) if ${FILTERED_OBJECTS}
-  end
+coreo_agent_rule_runner 'agent-rules' do
+  action :run
+  rules ${AUDIT_AGENT_RULES_ALERT_LIST}
+  profiles ${AUDIT_AGENT_PROFILES_ALERT_LIST}
+  filter(${FILTERED_OBJECTS}) if ${FILTERED_OBJECTS}
+end
